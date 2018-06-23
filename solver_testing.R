6ad9624bd0e95397a424d91aadbc36897a416d95
Philip Hossu <phossu@hawk.iit.edu>
2018-06-22 20:24:36 -0500

    Added a bunch of functions, re-structured and cleaned up code

--- solver_testing.R
+++ solver_testing.R
@@ -1,23 +1,50 @@
-library(mpoly); library(tidyverse)
+library(mpoly); library(tidyverse); library(kumerical); library(bertini)
 
-# First Setup  ===================================================
+# Define Functions  ===================================================
+
+# Generate roots of unity for solving start systems
+gen_roots_unity <- function(n){
+  unity_sols <- c()
+  for(k in 1:n){
+    unity_sols <- c(unity_sols,complex(1,real=cos((2*pi*k)/n),imaginary=sin((2*pi*k)/n)))
+  }
+  unity_sols
+}
+
+# Path tracker which utilizes kumerical's multinewton function for
+track_paths <- function(init,t_vals){
+  for(i in t_vals){
+    init <- multinewton(h(i),h_d(i),init)$root
+  }
+  init
+}
+
+# Generate Jacobian based on Dr. Kahle's function
+jacobian <- function(poly_system, varorder = vars(poly_system)) {
+  separate_mpolys <- lapply(poly_system, deriv, var = varorder)
+  gradients <- lapply(separate_mpolys, as.function, varorder = varorder, silent = TRUE)
+  J <- function(.) lapply(gradients, function(f) f(.))
+  function(v) do.call(rbind, J(v))
+}
+
+# Define Variables, Setup  ================================================
+
+# Create empty data frame for holding start system info
+df <- tibble(x=rep(0,length(target)),y=rep(0,length(target)),z=rep(0,length(target)))
+colnames(df) <- c("degree of nrow'th poly","startsys poly","startsys roots")
 
 # Creating a variables list so we know how many variables are used in target system
 vars <- c("x1","x2","x3","x4")
 
+# Defining random complex gamma for the homotopy equation
+gamma <- complex(1, runif(1), runif(1))
+
 # Enter target system as an mpoly object
-target <- mp(c("x1^3*x2-x1^2+x2-2","x1^4*x2-x2^3+2*x2-127"))
-target <- mp(c("x2-(x1^2)", "x2-(2 - x1^2)"))
-target <- mp(c("x2-(x1+1)","x2-x1^2"))
-target <- mp(c("x2-(x1+1)","x2-x1^2","x3-x1^2"))
+  # target <- mp(c("x1^3*x2-x1^2+x2-2","x1^4*x2-x2^3+2*x2-127"))
+  # target <- mp(c("x2-(x1^2)", "x2-(2 - x1^2)"))
+target <- mp(c("x1^2 + x2^2 - 1", "x1*x2 - 1"), varorder = vars)
 
-# Create empty data frame
-df <- tibble(x=rep(0,length(target)),y=rep(0,length(target)),z=rep(0,length(target)))
-colnames(df) <- c("degree of nrow'th poly","startsys poly","startsys roots")
-
-# Create & Solve Start System  ==================================
-
-# Fill first column with total degree of each polynomial corresponding to the row #
+# Fill first column of df with total degree of each polynomial corresponding to the row #
 df[1] <- mapply(totaldeg,target)
 
 # Write pretty start systems in column 2
@@ -27,127 +54,51 @@ while(i <= nrow(df)){
   i <- i + 1
 }
 
-# Setup matrix to hold information for start systems (for use in polyroot)
-for_start_solving <- matrix(0, nrow=max(df$`degree of nrow'th poly`)+1, ncol=length(target))
-for_start_solving[1,] <- -1
+# Define t-vals for continuation
+t_vals <- rev(seq(0, 1, 0.01))[-1]
 
-# Fill in support entries for polyroot to use in solving
-i <- 1
-while(i <= ncol(for_start_solving)){
-  for_start_solving[df$`degree of nrow'th poly`[i]+1,i] <- 1
-  i <- i + 1
-}
+# Solving Start System  ================================================
 
 # Solve start systems, store results in df
 i <- 1
 while(i <= ncol(for_start_solving)){
-  df$`startsys roots`[i] <- list(polyroot(for_start_solving[,i]))
+  df$`startsys roots`[i] <- list(gen_roots_unity(df$`degree of nrow'th poly`[i]))
   i <- i + 1
 }
 
-# df
-
 # Make combos of solutions, store in start_solutions and label
-start_solutions <- as.tibble(expand.grid(df$`startsys roots`))
+start_solutions <- expand.grid(df$`startsys roots`)
 colnames(start_solutions) <- c(vars[1:length(start_solutions)])
 
-# Setup Homotopy, Target Sys, Start Sys  ===============================
+# Setup Homotopy, Target, Start, Derivs as Functions  ===================
 
 # Convert target system to a R function using mpoly
-g <- as.function(target, varorder = vars)
+g <- as.function(target, varorder = vars(target))
+g_d <- jacobian(target)
 
 # Convert start system to mpoly object then to R function
-f <- as.function(mp(c(df$`startsys poly`)), varorder = vars)
+start_as_mp <- mp(c(df$`startsys poly`))
+f <- as.function(start_as_mp, varorder = vars(start_as_mp))
+f_gam <- function(.) gamma*f(.)
+
+f_d <- jacobian(start_as_mp)
+f_d_gam <- function(.) gamma*f_d(.)
 
 # Define Homotopy
-h <- function(t) function(v) (1-t)*g(v) + t*f(v)
+h <- function(t) function(v) (1-t)*g(v) + t*f_gam(v)
+h_d <- function(t) function(v) (1-t)*g_d(v) + t*f_d_gam(v)
 
-# Define t-vals
-t_vals <- rev(seq(0, 1, 0.05))[-1]
-start_solutions[as.character(c(t_vals))] <- NA
+# Solving  ==============================================================
 
-# Add column in start solutions tibble to allow for intermediate solution recording
-# start_solutions$tracking <- rep(list(0),nrow(start_solutions))
+(solutions <- apply(start_solutions, 1, track_paths, t_vals))
 
-# Testing  =========================================================
+# Check Solutions, Bertini  =============================================
 
-# Calculate derivatives using mpoly
+# Should be ~0 for all entries if solutions were successful
+apply(solutions, 1, g)
 
-g  <- function(x) c(x[1]^2 + x[2]^2 - 1, x[1]*x[2] - 1)
-dg <- function(x) matrix(c(
-  2*x[1], 2*x[2],
-  x[2],   x[1]
-), nrow = 2, ncol = 2, byrow = TRUE)
-
-g_mp <- mp(c("x1^2 + x2^2 - 1", "x1*x2 - 1"))
-
-(test <- lapply(g_mp,deriv,var=vars[1:nrow(df)]))
-# test <- as.mpoly(as.tibble(lapply(g_mp,deriv,var=vars[1:nrow(df)])))
-test
-as.mpoly(as.character(test[[1]]))
-mp(test[[1]][1])
-
-make_mp <- function(.){
-  mp(.[1],.[2],.[3],.[4])
-}
-
-# as.function(c(deriv(g_mp[[1]],var=vars[1:nrow(df)])))
-# test <- matrix(test,nrow=2,ncol=2)
-
-# test <- mp(unlist(test, recursive = TRUE))
-# as.function(test[,1])
-
-testFun <- function(x) matrix(c(lapply(test,as.function)),nrow=2,ncol=2)
-
-# CMD+SHIFT+C to remove comments
-
-# i <- 1;
-# for(i in 1:nrow(start_solutions)){
-#   init <- as.vector(unname(unlist(start_solutions[i,])))[1:nrow(df)]
-#   init_re <- init
-#   # init_re <- Re(init)
-#   # init_im <- Im(init)
-#   #j <- 1
-#   for (t in t_vals) {
-#     o_re <- optim(
-#       par = init_re,
-#       fn = function(x) h(t)(x) %>% abs () %>% sum()
-#     )
-#     #o_im <- optim(
-#     #  par = init_im,
-#     #  fn = function(x) h(t)(x) %>% abs () %>% sum()
-#     #)
-#     init_re <- o_re$par
-#     # init_im <- o_im$par
-#     #start_solutions[[i,j+2]] <- list(o$par)
-#     #j <- j + 1
-#   }
-#   #start_solutions[[i,j+2]] <- list(c(init[1:nrow(df)]))
-#   #start_solutions$'0'[i] <- list(complex(real=init_re, imaginary=init_im))
-#   start_solutions$'0'[i] <- list(c(init[1:nrow(df)]))
-#
-# }
-
-
-# View Solutions  =====================================================
-start_solutions$'0'
-unique(start_solutions$'0')
-
-start_solutions
-start_solutions$'0.9'
-
-# Check vs Bertini  ===================================================
-
-library(bertini)
-
-# target <- mp(c("x1^3*x2-x1^2+x2-2","x1^4*x2-x2^3+2*x2-127"))
-
-# lhs <- mp(c("x2-(x1+1)","x2-x1^2"))
-
+# Should be the same as the solutions we found
 rhs <- mp(c("0","0"))
-
-# poly_solve(lhs, rhs, c("x1","x2"))
-
 poly_solve(target, rhs, c("x1","x2"))
 
 
